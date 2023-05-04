c7b0d3101c4510d6f5bf66b9fb5a7e869855a2d0
JoooGomes <jjoao.10gomes@gmail.com>
2023-05-03 21:39:04 -0300

    aula01js

---
+++ João Carlos/WorkSpace/JS/main.js
@@ -0,0 +1,11 @@
+// Espaços em branco
+var numero1 = 3
+
+// texto
+var texto1 = "g" // caracter
+var texto2 = 'gggg' // string = texto
+
+// java char texto1 = 'g'
+// java String texto2 = "gggg"
+
+//
