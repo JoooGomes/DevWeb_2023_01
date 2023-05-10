5c4ecfbfa18115a5a20221fb1f7d773f61ace017
JoooGomes <jjoao.10gomes@gmail.com>
2023-05-09 21:41:53 -0300

    aula03js

---
+++ João Carlos/WorkSpace/JS/aula_03_js/exercicio/main.js
@@ -0,0 +1,13 @@
+var fruta01 = '  banana  '
+var fruta02 = 'Manga'
+var fruta03 = 'Abacaxi'
+var fruta04 = 'Caju'
+var fruta05 = 'Pera'
+
+
+const temp01 = fruta02.replace("Manga", "IOS");
+
+console.log(fruta03.length )
+console.log(fruta01.substring(1, 5))
+console.log(fruta04.trim())
+console.log(fruta05.toLowerCase())
