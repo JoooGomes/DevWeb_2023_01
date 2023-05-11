19e895f5e759e485cd28e1a6f7230036cf6569fe
JoooGomes <jjoao.10gomes@gmail.com>
2023-05-11 20:47:23 -0300

    aula05js

--- João Carlos/WorkSpace/JS/aula_05_js/pratica/main.js
+++ João Carlos/WorkSpace/JS/aula_05_js/pratica/main.js
@@ -20,4 +20,13 @@ text += `O número é ${i}\n`;
 i++;
 } while (i < 10);
 console.log(text);
-    
+// For
+console.clear();
+for (let i = 0; i < 10; i++) console.log(`Laço For número: ${i}`);
+// For - Decremento
+console.clear();
+for (let i = 10; i > 0; i--) console.log(`Laço For número: ${i}`);
+// Laços de arrays
+const frutas = ['apples', 'oranges', 'pears', 10, true];
+console.clear();
+for (let j = 0; j < frutas.length; j++) console.log(`Nome: ${frutas[j]} \n`);
