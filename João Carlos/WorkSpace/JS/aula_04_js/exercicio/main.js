7ff0b5f91edb8c48f1e29173ab7602838562aeea
JoooGomes <jjoao.10gomes@gmail.com>
2023-05-12 16:01:48 -0300

    att_das_aulas_js

--- João Carlos/WorkSpace/JS/aula_04_js/exercicio/main.js
+++ João Carlos/WorkSpace/JS/aula_04_js/exercicio/main.js
@@ -1,7 +1,11 @@
-var numero = 25215564213
 
-if (numero % 2  == 0) {
-        console.log(numero + " é par.");
-    } else {
-        console.log(numero + " é impar.");
-    }
+var numero1 = 16
+var numero2 = 68
+  
+ if (numero1 > numero2) {
+    console.log("numero1 é maior que numero2");
+  } else if (numero1 < numero2) {
+    console.log("numero1 é menor que numero2");
+  } else {
+    console.log("Os números são iguais");
+  }
