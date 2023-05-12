79bfe38d6682afb6b092d92b022ca8674b6aaa40
JoooGomes <jjoao.10gomes@gmail.com>
2023-05-12 15:43:26 -0300

    att_das_aulas_js

---
+++ João Carlos/WorkSpace/JS/aula_02_js/exercicio/main.js
@@ -0,0 +1,10 @@
+var nota1 = 4
+var nota2 = 5
+var nota3 = 7
+var nota4 = 10
+
+const media = (nota1 + nota2 + nota3 + nota4) / 4
+
+var resultado = "A media do aluno com as 4 notas é: " 
+
+console.log(resultado + media)
