3f7d746055f7982dabb7a1ba1e66973a8a46c9dd
JoooGomes <jjoao.10gomes@gmail.com>
2023-05-18 18:25:42 -0300

    aula08js

---
+++ João Carlos/WorkSpace/JS/aula_07_js/exercicio/main.js
@@ -0,0 +1,12 @@
+//Objetos
+var pessoa = {
+    nome: "João",
+    idade: 24,
+    altura: 1.69
+}
+
+
+console.table(pessoa)
+console.log(pessoa.nome)
+console.log(pessoa.idade)
+console.log(pessoa.altura)
