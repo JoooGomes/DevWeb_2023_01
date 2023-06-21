1e80ad4bd2ccce50fe08839f1bc3e8880c2c7326
JoooGomes <jjoao.10gomes@gmail.com>
2023-06-20 21:06:13 -0300

    aula03react

---
+++ João Carlos/WorkSpace/React/aula_03/tema_03_conta_cliques/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Exemplo from './Components/Exemplo';
+import Header from './Components/Header';
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Exemplo />
+    </div>
+  );
+}
+
+export default App;
