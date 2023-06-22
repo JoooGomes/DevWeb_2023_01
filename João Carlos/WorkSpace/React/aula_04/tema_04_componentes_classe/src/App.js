24747412bd66bb4fe86bd9c39c4ed455aae2720c
JoooGomes <jjoao.10gomes@gmail.com>
2023-06-21 21:28:48 -0300

    aula04react

---
+++ João Carlos/WorkSpace/React/aula_04/tema_04_componentes_classe/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Exemplo from './Components/ Exemplo';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Exemplo />
+    </div>
+  );
+}
+export default App; 
