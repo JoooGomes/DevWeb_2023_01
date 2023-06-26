f9e2dddf51525e6b3bcb4af0aa939307ddf55cf4
JoooGomes <jjoao.10gomes@gmail.com>
2023-06-26 19:13:51 -0300

    projetoaula05

--- João Carlos/WorkSpace/React/aula_05/tema_05_agenda/src/Components/Header.jsx
+++ João Carlos/WorkSpace/React/aula_05/tema_05_agenda/src/Components/Header.jsx
@@ -1,9 +1,24 @@
-const Header = () => {
- return (
- <header>
- <h1>Agenda de compromissos</h1>
- </header>
- );
- };
+import PropTypes from 'prop-types';
+import Button from './Button';
 
- export default Header;
+const Header = ({ title }) => {
+    const onClick = () => {
+        alert('Clicou');
+    };
+    return (
+        <header className="header">
+            <h1>Agenda de {title}</h1>
+            <Button bgColor="green" text="Add" onClick={onClick} />
+        </header>
+    );
+};
+
+Header.defaultProps = {
+    title: 'compromissos',
+};
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
+};
+
+export default Header;
